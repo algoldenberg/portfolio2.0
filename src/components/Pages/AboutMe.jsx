b3f1b495263f047c673005e774232b377c611f5e
algoldenberga <algoldenberga@gmail.com>
2023-02-21 18:36:04 +0200

    Transefered Student Projects Bar + cleared empty classes from jsx modules

--- src/components/Pages/AboutMe.jsx
+++ src/components/Pages/AboutMe.jsx
@@ -75,7 +75,7 @@ const AboutMe = () => {
             <Col xs={12} md={6} xl={7}>
               <TrackVisibility>
                 {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <div>
                   <span className="tagline">Welcome to my Portfolio!</span>
                   <h1>Hello! I'm <span className="purpleText">Aleks</span><br/> <span className="txt-rotate" dataPeriod="200" data-rotate='[ "Web Developer..."]'><span className="wrap">{text}</span></span></h1>
                     <p>Originally from Russia, I have been working in the automation of personnel training systems for several years. <br/>
--- src/components/Pages/StudentProjects.jsx
+++ src/components/Pages/StudentProjects.jsx
@@ -5,7 +5,6 @@ import "../../styles/Student.css"
 const StudentProjects = () => {
 const slides = document.querySelectorAll('.slide')
 const point = document.querySelectorAll('.point')
-const a = document.querySelectorAll('.projectText')
 
 
 for (const [i, slide] of slides.entries()) {
@@ -21,29 +20,72 @@ for (const [i, slide] of slides.entries()) {
 
 
 
+
 function clearActiveClasses() {
     slides.forEach((slide) => {
         slide.classList.remove('active')
     })
 }
 
+
     return (
         
         <div className='blockOne'>
-            <div className='slide'><span className="point">Drum Kit</span>
-                <p> <a href="./StudentWorks/drumKit/index.html" target="_blank" class="whiteText">&#10094;Drum Kit&#10095;</a> <br/><br/><br/><span class="projectText">Mission:   Make the audio file to make sounds 
-                    on click on the keyboard keys (one listener)
-                    and on mouse click on the buttons in the page 
-                    (one more listener)</span></p>
+            <div className='slide'>
+                <span className="point">Drum Kit</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Drum Kit&#10095;</a>
+                <span className="projectText">Mission:   Make the audio file to make sounds on click on the keyboard keys (one listener)and on mouse click on the buttons in the page (one more listener)</span>
+                </p>
             </div>
-            <div className='slide'></div>
-            <div className='slide'></div>
-            <div className='slide'></div>
-            <div className='slide'></div>
-            <div className='slide'></div>
-            <div className='slide'></div>
-            <div className='slide'></div>
-            <div className='slide'></div>
+            <div className='slide'>
+                <span className="point">Color The Parrot</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Color The Parrot&#10095;</a> <br/>
+                <span className="projectText">Mission:   Each button should change the color of 
+                    the respective parrot part</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Wheel Of Fortune</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Wheel Of Fortune&#10095;</a> <br/>
+                <span className="projectText">Mission:  Create wheel of fortune with "rotate" function</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Aspect Ratio</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Aspect Ratio&#10095;</a> <br/>
+                <span className="projectText">Mission:   Make aspect ratio calculator</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Postcards</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Postcards&#10095;</a> <br/>
+                <span className="projectText">Mission:   Create animated Postcards, when 1 card is opened, other should be minimized</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Grag&Drop Desk</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Grag&Drop Desk&#10095;</a> <br/>
+                <span className="projectText">Mission:  Using Drag & Drop functions make a project desk with text windows</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Photo Slider</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Photo Slider&#10095;</a> <br/>
+                <span className="projectText">Mission:   Make reverse Photo & Text slider with 5 or more blocks</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Aim Game</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Aim Game&#10095;</a> <br/>
+                <span className="projectText">Mission:   Make a game in which when you click on an element, it will read how many times you clicked on the element with 4 timers and changing color on click</span>
+                </p>
+            </div>
+            <div className='slide'>
+                <span className="point">Color Desk</span>
+                <p> <a href="#" target="_blank" className="whiteText">&#10094;Color Desk&#10095;</a> <br/>
+                <span className="projectText">Mission:   Create a board,which elements will be highlited with different colors when hovered</span>
+                </p>
+            </div>
         </div>
      );
 }
